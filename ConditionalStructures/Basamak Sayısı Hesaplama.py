b3aa735d01d4a926e724ea691fe1090d5278f226
Sercan Alan <sercan.alan@outlook.com>
2023-07-03 21:06:27 +0300

    Exercise Completed

--- ConditionalStructures/Basamak Sayısı Hesaplama.py
+++ ConditionalStructures/Basamak Sayısı Hesaplama.py
@@ -1,1 +1,14 @@
 # Maksimum 5 basamaklı üreteceğiniz bir rastgele sayının kaç basamaklı olduğunu hesaplayın.
+
+import random
+
+random_number = random.randint(1,99999)
+temp = random_number
+
+number_of_digit = 0
+
+while(temp != 0):
+        temp = temp // 10
+        number_of_digit +=1
+    
+print("Girilen {} sayısı {} basamaklıdır.".format(random_number,number_of_digit))
