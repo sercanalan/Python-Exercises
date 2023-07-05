1ba0d62a758ff873c1392f16cbd5180f0b782b71
Sercan Alan <sercan.alan@outlook.com>
2023-07-05 18:20:31 +0300

    Initial Commit

---
+++ Loops/For Döngüsü ile Cümledeki Boşluk Sayısını Hesaplama.py
@@ -0,0 +1,21 @@
+"""
+cumle="a bcd e f"
+
+1.İterasyon: Harf-> "a", boslukSayisi=0
+
+2.İterasyon: Harf-> " ", boslukSayisi=1
+
+3.İterasyon: Harf-> "b", boslukSayisi=1
+
+4.İterasyon: Harf-> "c", boslukSayisi=1
+
+5.İterasyon: Harf-> "d", boslukSayisi=1
+
+6.İterasyon: Harf-> " ", boslukSayisi=2
+
+7.İterasyon: Harf-> "e", boslukSayisi=2
+
+8.İterasyon: Harf-> " ", boslukSayisi=3
+
+9.İterasyon: Harf-> "f", boslukSayisi=3
+"""
