61b672d4de1785d53e80a889ed2dab61bf6602ad
Sercan Alan <sercan.alan@outlook.com>
2023-07-05 18:46:37 +0300

    Initial Commit

---
+++ Loops/Döngü İle Üçgen Çizdirme.py
@@ -0,0 +1,15 @@
+"""
+Örneğin üçgen limiti 6 ise;
+
+x            xxxxxx         xxxxxxxxxxx             x
+
+xx           xxxxx          xxxxxxxxx              xxx
+
+xxx          xxxx           xxxxxxx               xxxxx  
+
+xxxx         xxx            xxxxx                xxxxxxx 
+
+xxxxx        xx             xxx                 xxxxxxxxx
+
+xxxxxx       x              x                  xxxxxxxxxxx
+"""
