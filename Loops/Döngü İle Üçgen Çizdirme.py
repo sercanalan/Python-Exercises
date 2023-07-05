2766d731ea9cb79d05fea5e1909b552522d4c5b0
Sercan Alan <sercan.alan@outlook.com>
2023-07-05 19:03:35 +0300

    Sample Question Added

--- Loops/Döngü İle Üçgen Çizdirme.py
+++ Loops/Döngü İle Üçgen Çizdirme.py
@@ -1,15 +1,17 @@
 """
 Örneğin üçgen limiti 6 ise;
 
-x            xxxxxx         xxxxxxxxxxx             x
+   Triangle 1:                Triangle 2:              Triangle 3:                   Triangle 4: 
+    x       (1*)             xxxxxx  (6*)             xxxxxxxxxxx  (11*)                  x       (5 space, 1* , 5 space)  
 
-xx           xxxxx          xxxxxxxxx              xxx
+    xx      (2*)             xxxxx   (5*)             xxxxxxxxx    (9*)                  xxx      (4 space, 1* , 4 space)
 
-xxx          xxxx           xxxxxxx               xxxxx  
+    xxx     (3*)             xxxx    (4*)             xxxxxxx      (7*)                 xxxxx     (3 space, 1* , 3 space)
 
-xxxx         xxx            xxxxx                xxxxxxx 
+    xxxx    (4*)             xxx     (3*)             xxxxx        (5*)                xxxxxxx    (2 space, 1* , 2 space)
 
-xxxxx        xx             xxx                 xxxxxxxxx
+    xxxxx   (5*)             xx      (2*)             xxx          (3*)               xxxxxxxxx   (1 space, 1* , 1 space)
 
-xxxxxx       x              x                  xxxxxxxxxxx
+    xxxxxx  (6*)             x       (1*)             x            (1*)              xxxxxxxxxxx  (0 space, 1* , 0 space)
+        
 """
