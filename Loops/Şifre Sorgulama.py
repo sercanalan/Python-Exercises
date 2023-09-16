5bac4b0814d2fcb68ef6b9b72fdfb35731fe34cf
Sercan Alan <sercan.alan@outlook.com>
2023-09-16 13:46:02 +0300

    First Commit

---
+++ Loops/Şifre Sorgulama.py
@@ -0,0 +1,10 @@
+"""
+Koşul 1-> Şifrede hem büyük harf hem de küçük harf kullanılması gerek.
+
+Koşul 2-> Şifre içerisinde bir sayı kullanılmış olması gerek.
+
+Koşul 3-> Şifrenin 8 karakterden uzun olması gerek.
+
+Koşul 4-> Şifrenin 16 karakterden kısa olması gerek.
+
+"""
