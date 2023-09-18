fc70a1a7a6677008635a729da1d707c93f51de2f
Sercan Alan <sercan.alan@outlook.com>
2023-09-18 23:12:25 +0300

    Exercise Completed

--- Functions with Python/Listedeki En Büyük ve En Küçük N. Sayıyı Hesaplayan Fonksiyon.py
+++ Functions with Python/Listedeki En Büyük ve En Küçük N. Sayıyı Hesaplayan Fonksiyon.py
@@ -6,3 +6,21 @@ Fonksiyon dışarıdan bir liste ve bir N sayısı alacak, sırasıyla listenin 
 65 ve 23 sonuçlarını döndürecek
 
 """
+
+
+def largestN_SmallestN(enteredList,N):
+    enteredList.sort()
+
+    largestN = enteredList[-N]
+    smallestN = enteredList[N-1]
+    
+    return largestN,smallestN 
+
+
+largestN,smallestN = largestN_SmallestN([1,54,32,65,23,93],2)
+
+print("""
+        En Büyük N. Sayı: {}
+        En Küçük N. Sayı: {}
+
+     """.format(largestN,smallestN))
