d638cfca1559c1893f76cd17992b0ef75b36219c
Sercan Alan <sercan.alan@outlook.com>
2023-09-18 23:03:35 +0300

    Initial Commit

---
+++ Functions with Python/Listedeki En Büyük ve En Küçük N. Sayıyı Hesaplayan Fonksiyon.py
@@ -0,0 +1,8 @@
+"""
+Fonksiyon dışarıdan bir liste ve bir N sayısı alacak, sırasıyla listenin en büyük ve en küçük N. sayısını return edecek
+
+Örneğin liste: 1,54,32,65,23,93, N sayısı 2 ise
+
+65 ve 23 sonuçlarını döndürecek
+
+"""
