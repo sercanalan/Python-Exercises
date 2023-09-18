26c67fca8bba867d22916d3e5e40d9897ecc1128
Sercan Alan <sercan.alan@outlook.com>
2023-09-18 21:42:49 +0300

    Exercise Completed

--- Functions with Python/Ünlü Harf Sayısı Hesaplama Fonksiyonu.py
+++ Functions with Python/Ünlü Harf Sayısı Hesaplama Fonksiyonu.py
@@ -4,3 +4,24 @@ Fonksiyon dışarıdan bir string alacak ve stringteki ünlü harflerin sayısı
 unluHarfler=['a','e','i','u','o']
 
 """
+
+def counterOfVowels(str):
+    listOfStr = list(str)
+
+    numberOfVowels = 0
+
+    for x in range(len(listOfStr)):
+
+        if (listOfStr[x]=='a' or listOfStr[x]=='A' or
+            listOfStr[x]=='e' or listOfStr[x]=='E' or
+            listOfStr[x]=='i' or listOfStr[x]=='I' or
+            listOfStr[x]=='u' or listOfStr[x]=='U' or
+            listOfStr[x]=='o' or listOfStr[x]=='O'):
+            
+            numberOfVowels += 1
+
+    print("Sesli Harf Sayısı:",numberOfVowels)
+    
+
+enteredString = input("Bir Cümle Giriniz: ")
+counterOfVowels(enteredString)
